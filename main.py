76f17609e465dfadd6d0677f1d06e254ab0f4571
App Generator <support@appseed.us>
2022-01-20 17:37:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
 from fastapi import FastAPI
-
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 app = FastAPI()
-
-# Basic Hello World
 @app.get("/")
-async def root():
-    return {"message": "Hello FastAPI"}
-
-# Integer Validation
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-# Enum use
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+async def hello_world():
+    return {"hello": "world"}
